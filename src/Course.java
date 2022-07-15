db8d7074f528bdfee694f4f5d5cffdb7af6193ac
Rozmus <arozmus@deloittece.com>
2022-07-15 08:56:41 +0200

    Add class model

---
+++ src/Course.java
@@ -0,0 +1,37 @@
+package src;
+
+public class Course {
+    String name;
+    String lecturer;
+    int ects;
+
+    public Course(String name, String lecturer, int ects) {
+        this.name = name;
+        this.lecturer = lecturer;
+        this.ects = ects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLecturer() {
+        return lecturer;
+    }
+
+    public void setLecturer(String lecturer) {
+        this.lecturer = lecturer;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,57 @@
+package src;
+
+public class Student {
+    int id;
+    String name;
+    String lastName;
+    Faculty faculty;
+    Course course;
+
+    public Student(int id, String name, String lastName, Faculty faculty, Course course) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.faculty = faculty;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Faculty getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(Faculty faculty) {
+        this.faculty = faculty;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
